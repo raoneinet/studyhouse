8c55cb4936657ddff2e72ebca87d98f98d308b05
raoneinet <raoneinet@hotmail.com>
2025-12-07 17:03:54 +0000

    logobrand component

---
+++ src/components/header/logobrand.tsx
@@ -0,0 +1,14 @@
+
+type Props = {
+    title: string
+    subtitle: string
+}
+
+export const Logobrand = ({title, subtitle}: Props) => {
+    return (
+        <div>
+            <h3 className="text-neutral-800 font-bold text-3xl">{title}</h3>
+            <p className="text-neutral-600">{subtitle}</p>
+        </div>
+    )
+}
