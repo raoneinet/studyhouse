10226a701345613b5a1ff938b8f55ed867fac9f2
raoneinet <raoneinet@hotmail.com>
2025-12-07 22:03:43 +0000

    creating views

--- src/app/loggedIn/page.tsx
+++ src/app/loggedIn/page.tsx
@@ -1,7 +1,11 @@
+import { Navigation } from "@/ui/menu/navigation"
 export const LoggedIn = () => {
     return (
-        <div className="container mx-auto">
-            Logged In
+        <div className="container mx-auto flex flex-col md:flex-row gap-5">
+            <Navigation />
+            <div className=" bg-white my-5 rounded-lg p-3 flex-1">
+                Logged In
+            </div>
         </div>
     )
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,20 @@
 "use client"
-import {useState} from "react"
+import { useState } from "react"
 import { LoggedIn } from "./loggedIn/page"
 import { LoggedOut } from "./loggedOut/page"
 import { Header } from "@/components/header/header"
 
-const Page = ()=>{
+const Page = () => {
 
-  const [loggedIn, setLoggedIn] = useState<boolean>(false)
+    const [loggedIn, setLoggedIn] = useState<boolean>(true)
 
-  return (
-    <div className="bg-neutral-100 min-h-screen">
-      <Header/>
-      {loggedIn && <LoggedIn/>}
-      {!loggedIn && <LoggedOut/>}
-    </div>
-  )
+    return (
+        <div className="bg-neutral-100 min-h-screen">
+            <Header />
+            {loggedIn && <LoggedIn />}
+            {!loggedIn && <LoggedOut />}
+        </div>
+    )
 }
 
 export default Page
