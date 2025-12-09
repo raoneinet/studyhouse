0f7565a26c499ea4444453e5e1ef7113e23dd70e
raoneinet <raoneinet@hotmail.com>
2025-12-09 23:12:11 +0000

    creating form to add item

--- src/app/loggedIn/page.tsx
+++ src/app/loggedIn/page.tsx
@@ -1,10 +1,12 @@
 import { Navigation } from "@/ui/menu/navigation"
+import { CreateItem } from "@/ui/createItem/createItem"
+
 export const LoggedIn = () => {
     return (
         <div className="container mx-auto flex flex-col md:flex-row gap-5">
             <Navigation />
             <div className=" bg-white my-5 rounded-lg p-3 flex-1">
-                Logged In
+                <CreateItem/>
             </div>
         </div>
     )
