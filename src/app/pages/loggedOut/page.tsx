bd2856fc53a7ee3758706b56880329967c2b8e47
raoneinet <raoneinet@hotmail.com>
2025-12-10 23:57:12 +0000

    feat: creating menu

---
+++ src/app/pages/loggedOut/page.tsx
@@ -0,0 +1,11 @@
+import { LoginForm } from "../../ui/login/loginForm"
+
+export const LoggedOut = () => {
+    return (
+        <div className="container mx-auto">
+            <div>
+                Logged Out
+            </div>
+        </div>
+    )
+}
--- src/app/ui/menu/navigation.tsx
+++ src/app/ui/menu/navigation.tsx
@@ -1,20 +1,25 @@
 import { Button } from "@/components/ui/button"
+import { usePathname } from "next/navigation"
+import Link from "next/link"
 
 export const Navigation = () => {
+
+    const links = [
+        { href: "/", label: "Home" },
+        { href: "/createNewItem", label: "Novo Item" },
+        { href: "/allItems", label: "Todos os Itens" },
+    ]
     return (
         <div className="bg-white min-w-50 my-5 rounded-lg p-3">
             <nav>
-                <ul>
-                    <li>
-                        <Button variant="ghost" className="w-full">Home</Button>
-                    </li>
-                    <li>
-                        <Button variant="ghost" className="w-full">Novo Item</Button>
-                    </li>
-                    <li>
-                        <Button variant="ghost" className="w-full">Todos os Itens</Button>
-                    </li>
-                </ul>
+                {links.map(link => (
+                    <Link
+                        key={link.href}
+                        href={link.href}
+                    >
+                        <Button variant="ghost" className="w-full">{link.label}</Button>
+                    </Link>
+                ))}
             </nav>
         </div>
     )
