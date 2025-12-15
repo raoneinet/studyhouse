313072ab2981f7b079da464dc1b70fa6c2593582
raoneinet <raoneinet@hotmail.com>
2025-12-15 22:57:31 +0000

    fix: fixing login persists

--- src/context/userContext.tsx
+++ src/context/userContext.tsx
@@ -1,10 +1,12 @@
 "use client"
-import {User} from "@/types/user"
+
+import { User } from "@/types/user"
 import {
     createContext,
     useContext,
     useState,
-    ReactNode
+    ReactNode,
+    useEffect
 } from "react"
 
 type authContextType = {
@@ -21,20 +23,51 @@ const UserContext = createContext<authContextType>({
 
 export const ContextProvider = ({ children }: { children: ReactNode }) => {
 
-    const savedUser = localStorage.getItem("user")
-    
-    const [user, setUser] = useState<User | null>(() => {
-        return savedUser ? JSON.parse(savedUser) : null
-    })
+    const [user, setUser] = useState<User | null>(null)
 
-    const login = (userData: User) => {
-        setUser(userData)
+    useEffect(() => {
+        const checkSession = async () => {
+            try {
+                const res = await fetch("http://localhost/studyhouse_backend/api/me.php", {
+                    credentials: "include"
+                })
+
+                if (res.ok) {
+                    const data = await res.json()
+                    setUser(data.user)
+                } else {
+                    setUser(null)
+                }
+
+            } catch (error: any) {
+                setUser(null)
+                console.log("Erro ", error)
+            }
+        }
+
+        checkSession()
+    }, [])
+
+    const login = async (userData: User) => {
+        const res = await fetch("http://localhost/studyhouse_backend/api/me.php", {
+            credentials: "include"
+        });
+        const data = await res.json();
+        setUser(data.user);
     }
 
-    const logout = () => {
-        setUser(null)
-        localStorage.removeItem("token")
-        localStorage.removeItem("user")
+    const logout = async () => {
+        try {
+            await fetch("http://localhost/studyhouse_backend/api/logout.php", {
+                method: "POST",
+                credentials: "include"
+            })
+        } catch (error: any) {
+            console.log("Erro ao fazer logout. ", error)
+        } finally {
+            setUser(null)
+        }
+
     }
 
     return (
