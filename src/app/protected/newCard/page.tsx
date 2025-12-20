3dfef7cdacbbffdcd54cf1ad8d7d716c30269396
raoneinet <raoneinet@hotmail.com>
2025-12-20 18:09:16 +0000

    fix: form layout standard

--- src/app/protected/newCard/page.tsx
+++ src/app/protected/newCard/page.tsx
@@ -3,12 +3,14 @@ import { Title } from "@/components/title/title"
 
 const CreateNewItem = () => {
     return (
-        <div className="md:max-w-5xl">
+        <div className="md:max-w-[1009px]">
             <Title
                 title="Adicionar Novo Item"
                 style="text-2xl font-bold text-neutral-800 pb-5"
             />
-            <CreateItem />
+            <div className="bg-white border rounded-lg px-2 py-8">
+                <CreateItem />
+            </div>
         </div>
     )
 }
