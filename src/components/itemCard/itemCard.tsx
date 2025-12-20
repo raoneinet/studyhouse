a98a6243d5df8dc72d17a8f43d59ccdd1a981e4c
raoneinet <raoneinet@hotmail.com>
2025-12-20 11:43:43 +0000

    fix: tag mapping

--- src/components/itemCard/itemCard.tsx
+++ src/components/itemCard/itemCard.tsx
@@ -15,8 +15,6 @@ type Props = {
 
 export const ItemCard = ({ card, handleSelectCard }: Props) => {
 
-    const tags = card.tags.split(",")
-
     const statuses: StatusType[] = statusOptions.filter(opt => opt.id === card.status)
     const priority: PriorityType[] = priorityOptions.filter(opt => opt.id === card.priority)
 
@@ -43,7 +41,7 @@ export const ItemCard = ({ card, handleSelectCard }: Props) => {
                     </p>
                 </div>
                 <div className="flex gap-4 py-2">
-                    {tags.map((tag, index) => (
+                    {card.tags.split(",").map((tag, index) => (
                         <div
                             key={index}
                             className="px-2 py-1 bg-slate-100 text-slate-600 text-xs md:text-sm rounded-md"
