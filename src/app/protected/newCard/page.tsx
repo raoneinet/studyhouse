a37ff4d067e258229234bd99de21ad7b947545db
raoneinet <raoneinet@hotmail.com>
2025-12-22 00:03:19 +0000

    fix: fix pages titles

--- src/app/protected/newCard/page.tsx
+++ src/app/protected/newCard/page.tsx
@@ -5,7 +5,8 @@ const CreateNewItem = () => {
     return (
         <div className="md:max-w-[1009px]">
             <Title
-                title="Adicionar Novo Item"
+                title="Novo Assunto"
+                subtitle="Criar um novo assunto para estudar"
                 style="text-2xl font-bold text-neutral-800 pb-5"
             />
             <div className="bg-white border rounded-lg px-2 py-8">
--- src/components/title/title.tsx
+++ src/components/title/title.tsx
@@ -1,13 +1,15 @@
 
 type Props = {
     title: string
+    subtitle?: string
     style: string
 }
 
-export const Title = ({title, style}: Props)=>{
+export const Title = ({title, subtitle, style}: Props)=>{
     return (
         <div className={style}>
             <h2>{title}</h2>
+            <p className="text-sm font-normal text-slate-700">{subtitle}</p>
         </div>
     )
 }
