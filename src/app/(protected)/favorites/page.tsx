de3938be9534826db38a6e8266127904397726bb
raoneinet <raoneinet@hotmail.com>
2026-01-10 00:34:13 +0000

    quick fix

--- src/app/(protected)/favorites/page.tsx
+++ src/app/(protected)/favorites/page.tsx
@@ -31,14 +31,14 @@ const MyCards = () => {
             console.log("Erro ao buscar item favorito por ID. ", error)
         }
     }
-    const handleView = (view: boolean) => {
-        setViewList(view)
-    }
+    const handleView = (view: boolean) => setViewList(view)
+
+    const closeMobileModal = ()=> setSelectCard(null)
 
     useEffect(()=>{
         localStorage.setItem("favlist", JSON.stringify(viewList))
     }, [viewList])
-    
+
     return (
         <div className="md:max-w-full">
             <Title
@@ -75,9 +75,9 @@ const MyCards = () => {
                         </div>
                     }
                 </div>
-                <div className="hidden lg:block flex-1 min-w-0">
-                    <div className="sticky top-4 bg-white rounded-lg py-3 border">
-                        <ItemDetailSidebar selectCard={selectCard} />
+                <div className={`${selectCard ? "flex fixed top-0 right-0 bottom-0 left-0 scroll-y-hidden" : "hidden"} md:sticky lg:block md:flex-2 lg:flex-1 min-w-0 md:h-fit`}>
+                    <div className={`sticky top-4 bg-white rounded-lg py-3 border`}>
+                        <ItemDetailSidebar selectCard={selectCard} closeMobileModal={closeMobileModal}/>
                     </div>
                 </div>
             </div>
