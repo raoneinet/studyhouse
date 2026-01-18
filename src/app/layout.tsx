49fd5379179c920f196965c19682b5c51a0138d4
raoneinet <raoneinet@hotmail.com>
2026-01-18 22:36:46 +0000

    adding toaster to register form

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next"
 import "./globals.css"
 import { Providers } from "./providers/provider"
+import { Toaster } from "@/components/ui/sonner";
 
 export const metadata: Metadata = {
     title: "Estudaki",
@@ -18,6 +19,7 @@ export default function RootLayout({
                 <Providers>
                     {children}
                 </Providers>
+                <Toaster/>
             </body>
         </html>
     );
