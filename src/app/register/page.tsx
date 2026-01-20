484ec4218a345dab64eff63a3d5822de89177531
raoneinet <raoneinet@hotmail.com>
2026-01-20 23:05:36 +0000

    adding register obligation icon

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -7,7 +7,7 @@ const RegistrationPage = () => {
         <div className="w-full min-h-screen bg-linear-to-br from-purple-50 via-blue-50 to-indigo-50">
             <div className="container mx-auto min-h-screen flex flex-col justify-center items-center">
                 <Link href="/" className="place-self-start text-slate-600 p-3">← voltar</Link>
-                <div className="max-w-3xl bg-white px-4 py-6  flex flex-col gap-5 rounded-lg overflow-y-auto">
+                <div className="max-w-3xl bg-white px-4 py-6 my-6 flex flex-col gap-5 rounded-lg overflow-y-auto">
                     <div className="text-slate-800 cursor-pointer">
                         <Logobrand />
                         <p className="text-sm mt-3">Crie uma conta para organizar seus estudos de forma fácil</p>
@@ -15,6 +15,7 @@ const RegistrationPage = () => {
                     <div className="flex flex-col">
                         <RegisterForm />
                         <Link href="/login" className="text-sm text-slate-600 hover:underline mt-4">já tenho conta</Link>
+                        <p className="text-xs text-slate-600 pt-3">* Campos obrigatórios</p>
                     </div>
                 </div>
             </div>
