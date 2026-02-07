13497ad13dceeb1a39026dad84e00fcdff38ae64
raoneinet <raoneinet@hotmail.com>
2026-02-07 17:55:36 +0000

    validate form

--- src/utils/formSchema.ts
+++ src/utils/formSchema.ts
@@ -1,15 +1,15 @@
 import { z } from "zod"
 
 export const formSchema = z.object({
-    title: z.string().min(2),
+    title: z.string().trim().min(2, "Titulo deve ter mais de 2 caracteres"),
     links: z.array(
         z.object({
-            value: z.string().optional()
+            value: z.string().trim().optional()
         })
-    ),
-    description: z.string(),
-    category: z.string(),
-    status: z.string(),
-    tags: z.string(),
-    priority: z.string()
+    ).optional(),
+    description: z.string().trim().min(2, "Descrição não pode ficar vazio e deve ter mais de 2 caracteres"),
+    category: z.string("Obrigatório escolher uma categoria"),
+    status: z.string("Obrigatório escolher uma categoria"),
+    tags: z.string().optional(),
+    priority: z.string("Obrigatório escolher uma categoria")
 })
