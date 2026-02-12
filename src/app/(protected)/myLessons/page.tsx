11fb2ddfe94d852dc7303a7ed2fa84421e78ee80
raoneinet <raoneinet@hotmail.com>
2026-02-12 22:14:05 +0000

    quick fix on type

--- src/app/(protected)/myLessons/page.tsx
+++ src/app/(protected)/myLessons/page.tsx
@@ -28,7 +28,7 @@ const MyCards = () => {
     const handleSelectCard = async (id: number) => {
         try {
             const result = await triggerGetSubjectById(id).unwrap()
-            //setSelectCard(result)
+            setSelectCard(result)
         } catch (error) {
             console.log("Erro ao buscar por assunto por ID: ", error)
         }
@@ -37,9 +37,6 @@ const MyCards = () => {
     const handleView = (view: boolean) => setViewList(view)
     const closeMobileModal = ()=> setSelectCard(null)
 
-    useEffect(()=>{
-        setSelectCard(selectedCard as Subject)
-    }, [selectCard])
     useEffect(() => {
         localStorage.setItem("listview", JSON.stringify(viewList))
     }, [viewList])
