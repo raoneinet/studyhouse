58de7205f823db3cfc946996f78fceecae7fce5c
raoneinet <raoneinet@hotmail.com>
2026-03-01 18:18:51 +0000

    fix error request on link preview

--- src/components/linkPreview/linkPreview.tsx
+++ src/components/linkPreview/linkPreview.tsx
@@ -1,4 +1,5 @@
 "use client"
+import { ExternalLink } from "lucide-react"
 import { useEffect, useState } from "react"
 
 type Props = {
@@ -14,11 +15,12 @@ type Preview = {
 
 export const LinkPreview = ({ links }: Props) => {
     const [preview, setPreview] = useState<Preview>()
+    const [error, setError] = useState(false)
 
     console.log("Links: ", links)
 
     useEffect(() => {
-        const apiKey = process.env.LINK_PREVIEW_API
+        const apiKey = process.env.NEXT_PUBLIC_LINK_API
         const endpoint = `https://opengraph.io/api/1.1/site/${encodeURIComponent(links)}?app_id=${apiKey}`;
 
         async function fetchPreview() {
@@ -26,7 +28,12 @@ export const LinkPreview = ({ links }: Props) => {
                 const req = await fetch(endpoint);
                 const preview = await req.json();
 
-                console.log("Preview: ", preview.hybridGraph)
+                if (req.status != 200) {
+                    setError(true)
+                    return
+                }
+
+                console.log("Preview: ", req)
                 setPreview(preview.hybridGraph)
             } catch (error: any) {
                 console.log("Erro ao mostrar preview: ", error)
@@ -37,14 +44,24 @@ export const LinkPreview = ({ links }: Props) => {
     }, [])
 
     return (
-        <div className="flex gap-2">
-            <div className="w-10">
-                <img src={preview?.image} alt="Link preview of website" className="w-10 h-10"/>
-            </div>
-            <div className="flex-1">
-                <div className="font-bold text-xs">{preview?.title}</div>
-                <div className="text-xs line-clamp-2">{preview?.description}</div>
-            </div>
-        </div>
+        <>
+            {!error &&
+                <div className="flex gap-2">
+                    <div className="w-10">
+                        <img src={preview?.image} alt="Link preview of website" className="w-10 h-10" />
+                    </div>
+                    <div className="flex-1">
+                        <div className="font-bold text-xs">{preview?.title}</div>
+                        <div className="text-xs line-clamp-2">{preview?.description}</div>
+                    </div>
+                </div>
+            }
+            {error &&
+                <a href={links} target="_blank" className="flex gap-2 items-center">
+                    <ExternalLink className="w-4" />
+                    {links}
+                </a>
+            }
+        </>
     )
 }
