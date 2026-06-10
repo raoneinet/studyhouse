4b23d9bb3098a20ad053e0c3717c97c5f375fbaf
raoneinet <raoneinet@hotmail.com>
2026-06-10 18:10:49 +0100

    adding skeleton

---
+++ src/components/lessonCards/lessonDetailSidebarSkeleton.tsx
@@ -0,0 +1,59 @@
+import { Skeleton } from "@/components/ui/skeleton"
+import { Separator } from "../ui/separator"
+import {
+    SidebarGroup,
+    SidebarGroupLabel,
+    SidebarMenu,
+    SidebarContent,
+    SidebarGroupContent
+} from "../ui/sidebar"
+
+export const LessonDetailSidebarSkeleton = () => {
+    return (
+        <SidebarGroup>
+            <SidebarMenu className="flex flex-col justify-between">
+                <SidebarGroupLabel className="flex flex-row justify-between">
+                    <span className="text-slate-800 text-xl font-semibold">
+                        Detalhes de estudo
+                    </span>
+                    <Skeleton className="md:hidden h-6 w-6 rounded-md" />
+                </SidebarGroupLabel>
+                <Separator className="my-3" />
+            </SidebarMenu>
+            <SidebarContent>
+                <SidebarGroupContent className="p-4 space-y-6">
+                    {/* Header Details */}
+                    <div className="space-y-3">
+                        <div className="flex gap-2">
+                            <Skeleton className="h-6 w-20 rounded-full" />
+                            <Skeleton className="h-6 w-24 rounded-full" />
+                        </div>
+                        <Skeleton className="h-8 w-full rounded-md" />
+                        <Skeleton className="h-4 w-full rounded-md" />
+                        <Skeleton className="h-4 w-4/5 rounded-md" />
+                    </div>
+
+                    <Separator />
+
+                    {/* Meta info */}
+                    <div className="space-y-4">
+                        <div className="space-y-2">
+                            <Skeleton className="h-5 w-24 rounded-md" />
+                            <div className="flex gap-2">
+                                <Skeleton className="h-6 w-16 rounded-md" />
+                                <Skeleton className="h-6 w-20 rounded-md" />
+                            </div>
+                        </div>
+                        <div className="space-y-2">
+                            <Skeleton className="h-5 w-32 rounded-md" />
+                            <div className="space-y-2">
+                                <Skeleton className="h-8 w-full rounded-md" />
+                                <Skeleton className="h-8 w-full rounded-md" />
+                            </div>
+                        </div>
+                    </div>
+                </SidebarGroupContent>
+            </SidebarContent>
+        </SidebarGroup>
+    )
+}
